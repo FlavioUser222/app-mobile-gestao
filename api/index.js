0b4a2a68d6cd31ba1eea28cf33d211355d54eb92
FlavioUser222 <flavinhoth12@gmail.com>
2025-09-18 16:46:12 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -25,25 +25,99 @@ async function testConnection() {
 testConnection();
 
 
-app.post('/vendas', async (req, res) => {
-    const { cliente, valor, data } = req.body
+app.post('/cliente', async (req, res) => {
+    const { nome,  data ,quantidadeVendas} = req.body
 
     try {
         const result = await pool.query(
-            'INSERT INTO vendas (cliente, valor, data) VALUES ($1, $2, $3) RETURNING *',
-            [cliente, valor, data]
+            'INSERT INTO clientes (nome, data, quantidadeVendas) VALUES ($1, $2, $3) RETURNING *',
+            [nome, data,quantidadeVendas]
         )
-          res.status(201).json(result.rows[0]);
-
-
+        res.status(201).json(result.rows[0]);
     } catch (err) {
         console.error(err);
         res.status(500).json('Erro no server');
-
     }
-
-
 })
+
+
+app.get('/clientes', async (req, res) => {
+    try {
+        const result = await pool.query(
+            'SELECT * FROM clientes'
+        )
+        res.status(200).json(result.rows)
+    } catch (err) {
+        console.error(err);
+        res.status(500).json('Erro no server')
+    }
+})
+
+app.post('/venda', async (req, res) => {
+    const { cliente_id, quantidadeVendas, data, valor } = req.body
+
+    try {
+        const result = await pool.query(
+            'INSERT INTO vendas (cliente_id, quantidadeVendas, data,valor) VALUES ($1, $2, $3, $4) RETURNING *',
+            [cliente_id, quantidadeVendas, data, valor]
+        )
+
+        res.status(201).json(result.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json('Erro no server');
+    }
+})
+
+
+app.get('/vendas', async (req, res) => {
+    try {
+        const result = await pool.query(
+            `SELECT v.*, c.nome AS cliente_nome 
+             FROM vendas v 
+             JOIN clientes c ON v.cliente_id = c.id`
+        )
+        res.status(200).json(result.rows)
+    } catch (err) {
+        console.error(err);
+        res.status(500).json('Erro no server')
+    }
+})
+
+
+
+app.post('/despesa', async (req, res) => {
+    const { nome, valor, data } = req.body
+
+    try {
+        const result = await pool.query(
+            'INSERT INTO despesas (nome, valor, data) VALUES ($1, $2, $3) RETURNING *',
+            [nome, valor, data]
+        )
+        res.status(201).json(result.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json('Erro no server');
+    }
+})
+
+
+app.get('/despesas', async (req, res) => {
+    try {
+        const result = await pool.query(
+            'SELECT * FROM despesas'
+        )
+        res.status(200).json(result.rows)
+    } catch (err) {
+        console.error(err);
+        res.status(500).json('Erro no server')
+    }
+})
+
+
+
+
+
 
 app.listen(port, () => {
     console.log('App rodando na porta 3000')
