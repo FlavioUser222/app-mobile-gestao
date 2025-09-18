787b7a6abba32ea97add0961c53538c298a27c5f
FlavioUser222 <flavinhoth12@gmail.com>
2025-09-18 15:17:38 -0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,16 +1,42 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config();
 
 const port = 3000
-
+    
 const app = express()
 app.use(cors())
 app.use(express.json())
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL || 3000,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+async function testConnection() {
+    const res = await pool.query('SELECT NOW()');
+    console.log(res.rows[0]);
+}
+
+testConnection();
 
 
 app.post('/vendas', async (req, res) => {
     const { cliente, valor, data } = req.body
+
+    try {
+        const result = await pool.query(
+            'INSERT INTO vendas (cliente, valor, data) VALUES ($1, $2, $3) RETURNING *',
+            [cliente, valor, data]
+        )
+
+    } catch (err) {
+        console.error(err);
+    }
 
 
 })
