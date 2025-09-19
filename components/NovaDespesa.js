ee08b743dedb50ea8908e7d2cdc072dc3a17cf1a
FlavioUser222 <flavinhoth12@gmail.com>
2025-09-19 15:20:41 -0300

    Update NovaDespesa.js

--- components/NovaDespesa.js
+++ components/NovaDespesa.js
@@ -1,15 +1,52 @@
+import axios from 'axios';
 import { useState } from 'react';
-import { Text, TouchableOpacity, View, Image, Modal, TextInput } from 'react-native';
+import { Text, TouchableOpacity, View, Image, Modal, TextInput, FlatList } from 'react-native';
 
 export default function Cliente() {
 
     const [modal, setModal] = useState(false)
+    const [listaDespesa, setListaDespesa] = useState([])
+    let [nome, setNome] = useState('')
+    let [valor, setValor] = useState()
+    let [data, setData] = useState()
+
+
+    useState(() => {
+        async function fetchData() {
+            let res = await axios.get('https://app-mobile-gestao.onrender.com/despesas')
+
+            setListaDespesa(res.data)
+        }
+
+        fetchData()
+    }, [])
+
+
+
+    async function handleInputs() {
+        const novaDespesa = {
+            nome,
+            valor,
+            data
+        }
+        try {
+            let res = await axios.post('https://app-mobile-gestao.onrender.com/despesa', novaDespesa)
+        } catch (err) {
+
+            alert('Erro ao cadastrar despesa')
+        }
+
+    }
+
+
+
+
 
 
     return (
         <View>
             <View>
-                <TouchableOpacity onPress={()=>{setModal(true)}}>
+                <TouchableOpacity onPress={() => { setModal(true) }}>
                     <Text>Cadastrar nova despesa</Text>
                 </TouchableOpacity>
             </View>
@@ -29,14 +66,13 @@ export default function Cliente() {
                 transparent={true}>
                 <View>
                     <View>
-                        <TouchableOpacity onPress={()=>{setModal(false)}}><Text>X</Text></TouchableOpacity>
+                        <TouchableOpacity onPress={() => { setModal(false) }}><Text>X</Text></TouchableOpacity>
                     </View>
                     <View>
-                        <TextInput />
-                        <TextInput />
-                        <TextInput />
-                        <TextInput />
-                        <TouchableOpacity>
+                        <TextInput style={styles.input} value={nome} placeholder='Nome' onChangeText={(text) => { setNome(text) }} />
+                        <TextInput style={styles.input} value={valor} placeholder='Valor' onChangeText={(text) => { setValor(text) }} />
+                        <TextInput style={styles.input} value={data} placeholder='Data(XXXX-XX-XX)' onChangeText={(text) => { setData(text) }} />
+                        <TouchableOpacity onPress={() => { handleInputs() }}>
                             <Text>Cadastrar</Text>
                         </TouchableOpacity>
                     </View>
