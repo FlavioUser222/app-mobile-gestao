825485063e2d7e989080da6c8e2439dbc035969f
FlavioUser222 <flavinhoth12@gmail.com>
2025-09-19 16:24:08 -0300

    Update styles.js

--- styles/styles.js
+++ styles/styles.js
@@ -10,6 +10,27 @@ export const styles = StyleSheet.create({
         marginTop:50,
         height:120,
     },
+    modal:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+        padding:30,
+        backgroundColor:'#fff',
+    },
+    input:{
+        paddingVertical:10,
+        paddingHorizontal:10,
+        width:'100%',
+        backgroundColor:'#a19f9fff'
+    },
+    viewInput:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+        gap:10,
+    },
+
+
 
 
 
