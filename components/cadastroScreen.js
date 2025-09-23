27a3da902722c5cf873e5b0b9669887df780d4a1
FlavioUser222 <flavinhoth12@gmail.com>
2025-09-23 17:08:43 -0300

    Add files via upload

---
+++ components/cadastroScreen.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import { Text, TouchableOpacity, View, Image, Modal, TextInput, FlatList } from 'react-native';
+import axios from 'axios';
+import { styles } from '../styles/styles';
+import { Picker } from '@react-native-picker/picker'
+import { Feather, Ionicons } from 'react-native-vector-icons'
+
+
+export default function CadastrarUsuario() {
+    const [email, setEmail] = useState([])
+    const [senha, setSenha] = useState([])
+
+    async function postUsuario() {
+        let newUser = {
+            email,
+            senha
+        }
+        try {
+            let res = await axios.post("https://app-mobile-gestao.onrender.com/cadastrarUser", newUser)
+        } catch (error) {
+            res.send(500, "Erro ao cadastrar usuario")
+        }
+    }
+
+    return (
+        <View>
+            <View>
+                <TextInput value={email} onChangeText={(text) => { setEmail(text) }} />
+                <TextInput value={senha} onChangeText={(text) => { setSenha(text) }} />
+                <TouchableOpacity onPress={() => { postUsuario() }}>
+                    <Text>Cadastrar</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+
+
+
+
+
+
+
+
+}
