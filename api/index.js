a95730edc50bbe401bded26887b051af366bd4fd
FlavioUser222 <flavinhoth12@gmail.com>
2025-09-30 15:01:09 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -204,12 +204,14 @@ app.delete('/cliente/:id', async (req, res) => {
 
 app.get('/faturamentoTotal', async (req, res) => {
     const { usuario_id } = req.query
+
+
     try {
         const result = await pool.query(`
             SELECT 
-                COALESCE(SUM(valor), 0) AS faturamento_total
+                COALESCE(SUM(valor), 0) AS faturamento_total 
             FROM vendas 
-            WHERE usuario_id = $1
+            WHERE usuario_id = $1 AND foipaga = 'Recebida'
 `, [usuario_id]);
 
         res.status(200).json({ faturamento: result.rows[0].faturamento_total });
